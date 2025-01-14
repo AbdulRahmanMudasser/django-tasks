6132c4d495e2e3a5e8149f08fd5ef030421edfde
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-14 10:19:52 +0500

    todo model and database migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    srno = models.AutoField(auto_created=True, primary_key=True)
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    

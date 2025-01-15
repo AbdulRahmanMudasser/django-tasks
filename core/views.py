9cfec1f3a1a1bebc2ed1886cbc2f07e37c5d80c4
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-15 09:58:29 +0500

    login and signup view, url, template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from . import models
 
-# Create your views here.
+# Sign Up User View
+def signup(request):
+    # Check if Request Method is POST
+    if request.method == 'POST':
+        # Get Username from POST Method
+        username_ = request.POST.get('username')
+        
+        # Get Email from POST Method
+        email = request.POST.get('email')
+        
+        # Get Password from POST Method
+        password = request.POST.get('password')
+        
+        # Print User Details
+        print(username_, email, password)
+        
+        # Create New User
+        user = User.objects.create_user(username_, password)
+        
+        # Save User
+        user.save()
+        
+        # Redirect to Login Page]
+        return redirect('/login')
+    
+    # Render Sign Up Page
+    return render(request, 'signup.html')
+
+# Login User View
+def login(request):
+    # Render Login Page
+    return render(request, 'login.html')

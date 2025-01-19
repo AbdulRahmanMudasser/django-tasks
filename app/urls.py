b6d124b5ff3a0742ef20bd8e41b7564bef160ccf
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-19 12:31:49 +0500

    todo url, updated todo model, todo view and template

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,10 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('', views.signup_user, name='signup'),
+    path('login/', views.login_user, name='login'),
+    path('home/', views.home, name='home'),
+    
+    # Todo View URL
+    path('todo/', views.home, name='todo'),
 ]
--- core/models.py
+++ core/models.py
@@ -7,3 +7,6 @@ class Todo(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.title
+    

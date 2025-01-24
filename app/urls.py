c29e53afefdb1850c4a13442c9024d04f97ea1d4
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-24 10:32:50 +0500

    task creation & updation, templates, views, urls

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,15 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
+    # Admin Panel URL
     path('admin/', admin.site.urls),
+    
+    # Task List URL
     path('', views.task_list, name='task_list'),
+    
+    # Create Task URL
+    path('create/', views.task_create, name='task_create'),
+    
+    # Update Task URL
+    path('update/<int:pk>', views.task_update, name='task_update'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from . import models
 
 # All Tasks View
@@ -12,3 +12,51 @@ def task_list(request):
     
     # Render Task List Template
     return render(request, 'core/task_list.html', context)
+
+# Create Task View
+def task_create(request):
+    # Check if Request Method is POST
+    if request.method == 'POST':
+        # Get title from POST Request
+        title = request.POST.get('title')
+        
+        # Get description from POST Request
+        description = request.POST.get('description')
+        
+        # Create Task
+        models.Task.objects.create(title=title, description=description)
+        
+        # Redirect to Task List Template
+        return redirect('task_list')
+    
+    # Render Task Create Template
+    return render(request, 'core/task_form.html')
+
+# Update Task View
+def task_update(request, pk):
+    # Get Task With pk
+    task = get_object_or_404(models.Task, pk=pk)
+    
+    # Check if Request Method is POST
+    if request.method == 'POST':
+        # Get Updated title from POST Request
+        task.title = request.POST.get('title')
+        
+        # Get Updated description from POST Request
+        task.description = request.POST.get('description', '')
+        
+        # Get Task Completion status from POST Request
+        task.completed = 'completed' in request.POST
+        
+        # Save Task
+        task.save()
+        
+        # Redirect to Task List Template
+        return redirect('task_list')
+    
+    context = {
+        'task': task,
+    }
+    
+    # Render Task Update Template
+    return render(request, 'core/task_form.html', context)
